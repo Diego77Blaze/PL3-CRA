58952d2479fa318d36a245a698e78411fb3c1710
SergiodelaMata <sergio.matam@edu.uah.es>
2020-04-30 19:13:48 +0200

    Correct the function to get the cannonical reduction.

--- racionales.rkt
+++ racionales.rkt
@@ -3,20 +3,21 @@
 (require racket/include)
 (include "enteros.rkt")
 
-
-(define reduccanonica
+;; Obtiene la reducciona canónica de un número racional
+(define reduc_canonica
   (lambda (x)
     (lambda (y)
-      ((restoent x) y)
+      ;; Se obtiene el cociente de la división del dividendo como del divisor de la
+      ;; la fracción diviendolos ambos entre el mcd entre ambos
+      ((par (cocienteent x((mcdent x) y)))
+       (cocienteent y((mcdent x) y)))      
      )
    )
 )
 
-
-
 (define escero_racional
   (lambda (x)
-      (escero_racional ((reduccanonica (primero x)) (segundo x)))
+      (escero_racional ((reduc_canonica (primero x)) (segundo x)))
      )
 )
 
@@ -30,16 +31,22 @@
 (define suma_racional
   (lambda (num1)
     (lambda (num2)
-        (reduccanonica ((sument (prodent(primero num1)(mcment(segundo num1)(segundo num2)))(prodent(primero num2)(mcment(segundo num1)(segundo num2))))) (mcment(segundo num1)(segundo num2)))
+        (reduc_canonica
+         ((sument
+           (prodent(primero num1)(mcment(segundo num1)(segundo num2)))
+           (prodent(primero num2)(mcment(segundo num1)(segundo num2))))
+          )
+         (mcment(segundo num1)(segundo num2))
+         )
      )
    )
 )
 
-(define restarac
+(define resta_racional
   (lambda (num1)
     (lambda (num2)
       (lambda (y)
-        ((reduccanonica ((restarac num1) num2)) y)
+        ((reduc_canonica ((resta_racional num1) num2)) y)
        )
      )
    )
