4d7d36ccc7e0dddc634de19a037101e7a05aeb47
Diego77Blaze <48413305+Diego77Blaze@users.noreply.github.com>
2020-05-03 13:37:53 +0200

    PRACTICA C'EST FINI

--- racionales.rkt
+++ racionales.rkt
@@ -8,6 +8,8 @@
                       (list (testenteros (primero r)) (testenteros (segundo r)))))
 
 ;; Verifica si el número es 0
+;;(escero_racional ((par cero) uno))
+;;#<procedure:true>
 (define escero_racional
   (lambda (x)
     (esceroent (primero x))
@@ -15,6 +17,8 @@
   )
 
 ;; Verifica si el número no es 0
+;;(noescero_racional ((par cero) uno))
+;;#<procedure:false>
 (define noescero_racional
   (lambda (x)
     (neg(escero_racional (primero x)))
@@ -22,6 +26,8 @@
   )
 
 ;; Obtiene la reducciona canónica de un número racional positivo
+;;(test_racionales ((reduc_canonica dos)cuatro))
+;;(1 2)
 (define reduc_canonica
   (lambda (x)
     (lambda (y)
@@ -36,7 +42,10 @@
       )
     )
   )
+
 ;; Obtiene la suma de dos números racionales
+;;(test_racionales ((suma_racionales ((par uno) dos)) ((par uno) dos)))
+;;(1 1)
 (define suma_racionales
   (lambda (num1)
     (lambda (num2)
@@ -63,6 +72,8 @@
   )
 
 ;; Obtiene la resta de dos números racionales
+;;(test_racionales ((resta_racionales ((par siete) cinco)) ((par cuatro) cinco)))
+;;(3 5)
 (define resta_racionales
   (lambda (num1)
     (lambda (num2)
@@ -89,6 +100,8 @@
   )
 
 ;; Obtiene el producto de dos números racionales
+;;(test_racionales ((prod_racionales ((par uno) dos)) ((par cuatro) uno)))
+;;(2 1)
 (define prod_racionales
   (lambda (num1)
     (lambda (num2)
@@ -106,6 +119,8 @@
   )
 
 ;; Obtiene la división de dos números racionales
+;;(test_racionales ((prod_racionales ((par cuatro) dos)) ((par cuatro) uno)))
+;;(8 1)
 (define div_racionales
   (lambda (num1)
     (lambda (num2)
@@ -123,6 +138,8 @@
   )
 
 ;; Obtiene la fracción invertida
+;;(test_racionales (inverso_racionales ((par tres) siete)))
+;(7 3)
 (define inverso_racionales
   (lambda (num)
     ((reduc_canonica
@@ -133,6 +150,8 @@
   )
 
 ;; Verifica si una fracción es mayor que otra
+;;((mayor_racional ((par tres) dos)) ((par dos) dos))
+;;#<procedure:true>
 (define mayor_racional
   (lambda (num1)
     (lambda (num2)
@@ -154,7 +173,10 @@
       )
     )
   )
+
 ;; Verifica si una fracción es menor que otra
+;;((menor_racional ((par tres) dos)) ((par dos) dos))
+;;#<procedure:false>
 (define menor_racional
   (lambda (num1)
     (lambda (num2)
@@ -177,6 +199,8 @@
     )
   )
 ;; Verifica si dos números racionales son iguales
+;;((esigual_racional ((par dos) dos)) ((par dos) dos))
+;;#<procedure:true>
 (define esigual_racional
   (lambda (num1)
     (lambda (num2)
@@ -218,8 +242,15 @@
         (lambda (d)
           ((par ((par a) b)) ((par c) d))))))
   )
+(define identidad ((((definir_matriz ((par uno) uno)) ((par cero) uno)) ((par cero) uno)) ((par uno) uno)))
+(define matriz_nula ((((definir_matriz ((par cero) uno)) ((par cero) uno)) ((par cero) uno)) ((par cero) uno)))
+(define matriz_prueba1 ((((definir_matriz ((par dos) cuatro)) ((par cuatro) cuatro)) ((par -uno) cuatro)) ((par cinco) cuatro)))
+(define matriz_prueba2 ((((definir_matriz ((par uno) cuatro))   ((par -cuatro) seis))     ((par dos) ocho)) ((par -dos) tres)))
+(define matriz_prueba3 ((((definir_matriz ((par uno) dos))   ((par -cuatro) dos))     ((par dos) dos)) ((par -tres) dos)))
 
 ;; Test para comprobar operaciones con matrices
+;;(test_matriz identidad)
+;;(((1 1) (0 1)) ((0 1) (1 1)))
 (define test_matriz
   (lambda (m)
     (list (list (test_racionales (primero (primero m))) (test_racionales (segundo (primero m))))
@@ -229,6 +260,8 @@
   )
 
 ;; Realiza la suma de dos matrices 2x2
+;;(test_matriz ((suma_matrices matriz_prueba1) matriz_prueba2))
+;;(((3 4) (1 3)) ((0 1) (7 12)))
 (define suma_matrices
   (lambda (matriz1)
     (lambda (matriz2)
@@ -242,6 +275,8 @@
   )
 
 ;; Realiza la resta de dos matrices 2x2
+;;(test_matriz ((resta_matrices matriz_prueba1) matriz_prueba2))
+;;(((1 4) (5 3)) ((-1 2) (23 12)))
 (define resta_matrices
   (lambda (matriz1)
     (lambda (matriz2)
@@ -255,6 +290,8 @@
   )
 
 ;; Realiza el producto de dos matrices 2x2
+;;(test_matriz ((prod_matrices matriz_prueba1) matriz_prueba2))
+;;(((3 8) (-1 1)) ((1 4) (-2 3)))
 (define prod_matrices
   (lambda (matriz1)
     (lambda (matriz2)
@@ -276,6 +313,8 @@
   )
 
 ;; Realiza el cuadrado de una matriz
+;;(test_matriz (cuadrado_matrices matriz_prueba1))
+;;(((0 1) (7 4)) ((-7 16) (21 16)))
 (define cuadrado_matrices
   (lambda (matriz)
     ((prod_matrices matriz) matriz)
@@ -309,6 +348,7 @@
 
 ;; Verifica si una matriz es invertible o no
 ;; (inversa? identidad)
+;;#<procedure:true>
 (define inversa?
   (lambda (matriz)
     (((escero_racional ((reduc_canonica (primero(determinante matriz))) (segundo(determinante matriz))))
@@ -369,7 +409,9 @@
   )
 
 ;; Realiza la potencia de una matriz
-(define potecia_matricesaux
+;;(test_matriz ((potencia_matricesaux matriz_prueba1) deux))
+;;(((0 1) (7 4)) ((-7 16) (21 16)))
+(define potencia_matricesaux
     (lambda (matriz)
         (lambda (num)
             ((Y (lambda (f)
@@ -400,13 +442,15 @@
       )
   )
 
-;; Realiza la potencia de una matriz 
+;; Realiza la potencia de una matriz
+;;(test_matriz ((potencia_matrices matriz_prueba1) deux)) 
+;;(((0 1) (7 4)) ((-7 16) (21 16)))
 (define potencia_matrices
   (lambda (matriz)
     (lambda (num)
       (((escero num)
         (lambda (no_use) matriz)
-        (lambda (no_use) ((potecia_matricesaux matriz) num))) zero)
+        (lambda (no_use) ((potencia_matricesaux matriz) num))) zero)
       )
     )
   )
